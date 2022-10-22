a8e2ef102f05c193f269b228b1f8ec2147696d02
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2022-10-22 14:22:41 +0300

    Лаб 18.5. Добавил методы для извлечения вершин

--- lab815/src/lib/BinaryHeap.h
+++ lab815/src/lib/BinaryHeap.h
@@ -20,7 +20,7 @@ public:
 		int curPos = m_values.size() - 1;
 		int parentPos = (curPos - 1) / 2;
 
-		while (curPos > 0 && m_values[curPos] > m_values[parentPos])
+		while (curPos > 0 && m_values[parentPos] < m_values[curPos])
 		{
 			std::swap(m_values[parentPos], m_values[curPos]);
 
@@ -29,8 +29,60 @@ public:
 		}
 	}
 
+	T Top() const
+	{
+		return m_values[0];
+	}
+
+	void Pop()
+	{
+		if (m_values.empty())
+		{
+			return;
+		}
+
+		m_values[0] = m_values[m_values.size() - 1];
+		m_values.pop_back();
+
+		SiftUp(0);
+	}
+
 private:
 	std::vector<T> m_values;
+
+	void SiftUp(std::size_t curPos)
+	{
+		if (m_values.empty())
+		{
+			return;
+		}
+
+		while (true)
+		{
+			auto leftPos = curPos * 2 + 1;
+			auto rightPos = curPos * 2 + 2;
+
+			auto largestPos = curPos;
+
+			if (leftPos < m_values.size() && m_values[largestPos] < m_values[leftPos])
+			{
+				largestPos = leftPos;
+			}
+
+			if (rightPos < m_values.size() && m_values[largestPos] < m_values[rightPos])
+			{
+				largestPos = rightPos;
+			}
+
+			if (largestPos == curPos)
+			{
+				break;
+			}
+
+			std::swap(m_values[curPos], m_values[largestPos]);
+			curPos = largestPos;
+		}
+	}
 };
 
 #endif // BINARYHEAP_H
